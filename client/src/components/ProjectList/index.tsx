1650b902defe8cd7c9517071ef7edd5d779bb76a
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-29 18:05:33 +0900

    feat: ProjectListにfetchProjectsの実装

--- client/src/components/ProjectList/index.tsx
+++ client/src/components/ProjectList/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useFirestore } from '../../libs/db/useFirestore';
 import { focus, Project } from '../../redux/modules/project';
 import { Task } from '../../redux/modules/task';
 import { ProjectList as Presentational } from './ProjectList';
@@ -11,10 +12,16 @@ type Props = {
 };
 export const ProjectList: React.VFC<Props> = ({ context, projects, tasks }) => {
   const dispatch = useDispatch();
+  const { fetchProjects } = useFirestore();
 
   const handleFocus = (id: string) => {
     dispatch(focus({ id }));
   };
+
+  useEffect(() => {
+    fetchProjects();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Presentational
