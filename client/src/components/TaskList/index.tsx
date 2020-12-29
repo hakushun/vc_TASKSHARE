a00c1b0f3b7d1ace9cde6ef99aebbf40c1470bfc
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-29 18:06:33 +0900

    feat: TaskListにfetchTasksの実装

--- client/src/components/TaskList/index.tsx
+++ client/src/components/TaskList/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useFirestore } from '../../libs/db/useFirestore';
 import { focus, Task } from '../../redux/modules/task';
 import { TaskList as Presentational } from './TaskList';
 
@@ -9,10 +10,17 @@ type Props = {
 };
 export const TaskList: React.VFC<Props> = ({ context, tasks }) => {
   const dispatch = useDispatch();
+  const { fetchTasks } = useFirestore();
 
   const handleFocus = (taskId: string, projectId: string) => {
     dispatch(focus({ id: taskId, projectId }));
   };
+
+  useEffect(() => {
+    fetchTasks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Presentational context={context} tasks={tasks} handleFocus={handleFocus} />
   );
