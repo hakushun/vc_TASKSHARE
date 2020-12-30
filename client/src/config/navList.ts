f25580cfb13ced50fde82c836aabac5d11f2b88f
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-30 09:56:36 +0900

    chore: 不要ファイルの削除

--- client/src/config/navList.ts
+++
@@ -1,5 +0,0 @@
-export const navList = [
-  { id: 1, href: '/mypage', label: 'My Page' },
-  { id: 2, href: '/projects', label: 'Project List' },
-  { id: 3, href: '/tasks', label: 'Task List' },
-];
