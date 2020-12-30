0b3b45e12e32d534426540ee86ed131e46d07806
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-30 10:48:41 +0900

    feat: ハンバーガーメニュー実装

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from './index.module.scss';
+import { BargerMenu } from '../BargerMenu';
 
 type Props = {
   isAuth: boolean;
   logout: () => Promise<void>;
+  handletoggle: () => void;
 };
-export const Header: React.VFC<Props> = ({ isAuth, logout }) => (
+export const Header: React.VFC<Props> = ({ isAuth, logout, handletoggle }) => (
   <header className={styles.header}>
     <div className={styles.inner}>
       <h1 className={styles.title}>
@@ -19,13 +21,13 @@ export const Header: React.VFC<Props> = ({ isAuth, logout }) => (
           {!isAuth ? (
             <>
               <li className={styles.navItem}>
-                <Link href="/signup">
-                  <a className={styles.navLink}>SignUp</a>
+                <Link href="/signin">
+                  <a className={styles.navLink}>SignIn</a>
                 </Link>
               </li>
               <li className={styles.navItem}>
-                <Link href="/signin">
-                  <a className={styles.navLink}>SignIn</a>
+                <Link href="/signup">
+                  <a className={styles.navLink}>SignUp</a>
                 </Link>
               </li>
             </>
@@ -58,6 +60,19 @@ export const Header: React.VFC<Props> = ({ isAuth, logout }) => (
           )}
         </ul>
       </nav>
+      <button
+        className={styles.menuButton}
+        type="button"
+        onClick={() => handletoggle()}>
+        <img
+          src="/images/icon-menu.svg"
+          alt="メニューボタン"
+          width="44"
+          height="44"
+          className={styles.menu}
+        />
+      </button>
+      <BargerMenu />
     </div>
   </header>
 );
--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectIsAuth } from '../../redux/modules/user';
 import { Header as Presentational } from './Header';
 import { useAuth } from '../../libs/auth/useAuth';
+import { toggleBargerMenu } from '../../redux/modules/bargerMenu';
 
 export const Header: React.VFC = () => {
+  const dispatch = useDispatch();
+
   const isAuth = useSelector(selectIsAuth);
   const { logout } = useAuth();
 
-  return <Presentational isAuth={isAuth} logout={logout} />;
+  const handletoggle = () => {
+    dispatch(toggleBargerMenu());
+  };
+  return (
+    <Presentational
+      isAuth={isAuth}
+      logout={logout}
+      handletoggle={handletoggle}
+    />
+  );
 };
