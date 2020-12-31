dd3606da3c3d41fa9f0b1f29a876dd426cb55567
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-31 12:58:20 +0900

    feat: 作成したmoduleをimport

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,22 @@
-import {  Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
-
+import { ActivitiesModule } from './activities/activities.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ProjectsModule, TasksModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ProjectsModule,
+    TasksModule,
+    UsersModule,
+    ActivitiesModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
