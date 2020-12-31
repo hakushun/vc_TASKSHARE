f1f2d85b104452d73e9efbd9b933437088727876
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-31 12:58:41 +0900

    format: lint fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3000',
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, preflight',
-    credentials: true
+    credentials: true,
   });
 
   await app.listen(4000);
