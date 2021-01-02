7db0d4de47a9840b3106b9e4813f0acb6b012a19
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-02 11:11:11 +0900

    feat: taskのcrudメソッド作成

--- client/src/libs/db/crud.ts
+++ client/src/libs/db/crud.ts
@@ -5,6 +5,11 @@ import {
   UpdatePayload as UpdateProject,
 } from '../../redux/modules/projects';
 import {
+  CreatePayload as CreateTask,
+  RemovePayload as RemoveTask,
+  UpdatePayload as UpdateTask,
+} from '../../redux/modules/tasks';
+import {
   CreatePayload as CreateUser,
   RemovePayload as RemoveUser,
   UpdatePayload as UpdateUser,
@@ -65,4 +70,28 @@ export const putProject = async (data: UpdateProject): Promise<void> => {
 };
 export const deleteProject = async (data: RemoveProject): Promise<void> => {
   await db.collection('projects').doc(data.id).delete();
+};
+
+// CRUD Tasks
+export const postTask = async (data: CreateTask): Promise<void> => {
+  const task = {
+    ...data,
+    createdAt: new Date().getTime(),
+    updatedAt: new Date().getTime(),
+  };
+  const docRef = await db.collection('tasks').add({ ...task });
+  await db.collection('tasks').doc(docRef.id).update({ id: docRef.id });
+};
+export const putTask = async (data: UpdateTask): Promise<void> => {
+  const task = {
+    ...data,
+    updatedAt: new Date().getTime(),
+  };
+  await db
+    .collection('tasks')
+    .doc(task.id)
+    .set({ ...task }, { merge: true });
+};
+export const deleteTask = async (data: RemoveTask): Promise<void> => {
+  await db.collection('tasks').doc(data.id).delete();
 };
