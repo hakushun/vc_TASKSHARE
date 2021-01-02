d59de1696c239a295bf8c1ee04295ce97411f1de
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-02 10:54:38 +0900

    feat: projectのcrudメソッド作成

--- client/src/libs/db/crud.ts
+++ client/src/libs/db/crud.ts
@@ -1,5 +1,10 @@
 import { getInstance } from './getInstance';
 import {
+  CreatePayload as CreateProject,
+  RemovePayload as RemoveProject,
+  UpdatePayload as UpdateProject,
+} from '../../redux/modules/projects';
+import {
   CreatePayload as CreateUser,
   RemovePayload as RemoveUser,
   UpdatePayload as UpdateUser,
@@ -37,4 +42,27 @@ export const putUser = async (data: UpdateUser): Promise<void> => {
 
 export const deleteUser = async (data: RemoveUser): Promise<void> => {
   await db.collection('users').doc(data.id).delete();
+};
+// CRUD Projects
+export const postProject = async (data: CreateProject): Promise<void> => {
+  const project = {
+    ...data,
+    createdAt: new Date().getTime(),
+    updatedAt: new Date().getTime(),
+  };
+  const docRef = await db.collection('projects').add({ ...project });
+  await db.collection('projects').doc(docRef.id).update({ id: docRef.id });
+};
+export const putProject = async (data: UpdateProject): Promise<void> => {
+  const project = {
+    ...data,
+    updatedAt: new Date().getTime(),
+  };
+  await db
+    .collection('projects')
+    .doc(project.id)
+    .set({ ...project }, { merge: true });
+};
+export const deleteProject = async (data: RemoveProject): Promise<void> => {
+  await db.collection('projects').doc(data.id).delete();
 };
