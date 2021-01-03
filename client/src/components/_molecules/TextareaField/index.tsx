c10d8b7cefbd33abdac105295b27830622e5107b
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-03 19:39:25 +0900

    refactor: TextareaFieldコンポーネントの作成

---
+++ client/src/components/_molecules/TextareaField/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { InputLabel } from '../../_atoms/InputLabel';
+import { OptionalBadge } from '../../_atoms/OptionalBadge';
+import { RequiredBadge } from '../../_atoms/RequiredBadge';
+import { Textarea } from '../../_atoms/Textarea';
+import styles from './index.module.scss';
+
+type Props = {
+  label: string;
+  name: string;
+  id: string;
+  placeholder: string;
+  disabled: boolean;
+  required: boolean;
+};
+export const TextareaField: React.VFC<Props> = ({
+  label,
+  name,
+  id,
+  placeholder,
+  disabled,
+  required,
+}) => (
+  <div className={styles.root}>
+    <div className={styles.inner}>
+      <InputLabel id={id} label={label} />
+      {required ? <RequiredBadge /> : <OptionalBadge />}
+    </div>
+    <Textarea
+      name={name}
+      id={id}
+      placeholder={placeholder}
+      disabled={disabled}
+      required={required}
+    />
+  </div>
+);
