b97b830dd72b8aeefdacc1f954ece7011e3c0fea
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-03 17:29:59 +0900

    refactor: Headingコンポーネントの作成

---
+++ client/src/components/_molecules/Heading/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+type Props = {
+  title: string;
+};
+export const Heading: React.FC<Props> = ({ title, children }) => (
+  <div className={styles.root}>
+    <h2 className={styles.title}>{title}</h2>
+    {children}
+  </div>
+);
