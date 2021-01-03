64e65144d26ee0d069f9833c136853f4dc34c064
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-03 20:55:28 +0900

    refactor: TaskOverviewコンポーネントの作成

---
+++ client/src/components/_molecules/TaskOverview/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import clsx from 'clsx';
+import styles from './index.module.scss';
+import { selectTask } from '../../../redux/modules/task';
+import { getStringDate } from '../../../libs/date';
+import {
+  selectAssignUser,
+  selectUserCreateTask,
+} from '../../../redux/modules/users';
+
+export const TaskOverview: React.VFC = () => {
+  const task = useSelector(selectTask);
+  const assignUer = useSelector(selectAssignUser);
+  const createUer = useSelector(selectUserCreateTask);
+
+  return (
+    <div className={styles.inner}>
+      <dl className={clsx(styles.item, styles.description)}>
+        <dt className={styles.label}>Description</dt>
+        <dd className={styles.detail}>{task.description}</dd>
+      </dl>
+      <dl className={clsx(styles.item, styles.assignTo)}>
+        <dt className={styles.label}>Assign to</dt>
+        <dd className={styles.definition}>
+          {assignUer?.username || 'undefined'}
+        </dd>
+      </dl>
+      <dl className={clsx(styles.item, styles.createdBy)}>
+        <dt className={styles.label}>Created By</dt>
+        <dd className={styles.definition}>
+          {createUer?.username || 'undefined'}
+        </dd>
+      </dl>
+      <dl className={clsx(styles.item, styles.startDate)}>
+        <dt className={styles.label}>Start Date</dt>
+        <dd className={styles.definition}>{getStringDate(task.startDate)}</dd>
+      </dl>
+      <dl className={clsx(styles.item, styles.dueDate)}>
+        <dt className={styles.label}>Due Date</dt>
+        <dd className={styles.definition}>{getStringDate(task.dueDate)}</dd>
+      </dl>
+      <dl className={clsx(styles.item, styles.createdAt)}>
+        <dt className={styles.label}>Created at</dt>
+        <dd className={styles.definition}>{getStringDate(task.createdAt!)}</dd>
+      </dl>
+      <dl className={clsx(styles.item, styles.updatedAt)}>
+        <dt className={styles.label}>Updated at</dt>
+        <dd className={styles.definition}>{getStringDate(task.updatedAt!)}</dd>
+      </dl>
+    </div>
+  );
+};
