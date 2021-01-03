e04637bb8bc7b47f5a0d11bcce53d7bc15885aa0
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-03 16:25:08 +0900

    refactor: Selectboxコンポーネントの作成

---
+++ client/src/components/_atoms/Selectbox/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Field } from 'react-final-form';
+import clsx from 'clsx';
+import styles from './index.module.scss';
+import { composeValidators, isRequired } from '../../../libs/validations';
+
+type Props = {
+  name: string;
+  id: string;
+};
+export const Selectbox: React.FC<Props> = ({ name, id, children }) => (
+  <div className={styles.root}>
+    <Field
+      name={name}
+      validate={composeValidators(isRequired)}
+      subscription={{
+        value: true,
+        active: true,
+        error: true,
+        touched: true,
+      }}>
+      {({ input, meta }) => (
+        <select
+          id={id}
+          className={clsx(
+            styles.selectbox,
+            meta.touched && meta.error && styles.hasError,
+          )}
+          required
+          aria-required
+          {...input}>
+          {children}
+        </select>
+      )}
+    </Field>
+  </div>
+);
