6b9108f81cd442ac2d3ae01fe1bcfc4b825864dc
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-01-03 15:13:35 +0900

    chore: 不要ファイルの削除

--- client/src/components/Badge/Required.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './index.module.scss';
-
-export const Required: React.VFC = () => (
-  <span className={clsx(styles.badge, styles.required)}>Required</span>
-);
