3a2b5310e22d3167b84739788ddd107f64d1d725
Shun Nakano <nakano_syun@waku-2.com>
2021-01-04 16:08:50 +0900

    refactor: StatusListButtonの適用

--- client/src/components/TaskStatusList/TaskStatusList.tsx
+++ client/src/components/TaskStatusList/TaskStatusList.tsx
@@ -1,43 +1,12 @@
 import React from 'react';
-import { TaskStatus } from '../../redux/modules/task';
+import { StatusListButton } from '../_atoms/StatusListButton';
 import styles from './index.module.scss';
 
-type Props = {
-  updateTaskStatus: (_status: TaskStatus) => void;
-};
-export const TaskStatusList: React.VFC<Props> = ({ updateTaskStatus }) => (
-  <ul className={styles.statusList}>
-    <li className={styles.statusItem}>
-      <button
-        type="button"
-        className={styles.statusButton}
-        onClick={() => updateTaskStatus('NEW')}>
-        New
-      </button>
-    </li>
-    <li className={styles.statusItem}>
-      <button
-        type="button"
-        className={styles.statusButton}
-        onClick={() => updateTaskStatus('IN_PROGRESS')}>
-        WIP
-      </button>
-    </li>
-    <li className={styles.statusItem}>
-      <button
-        type="button"
-        className={styles.statusButton}
-        onClick={() => updateTaskStatus('REVIEWING')}>
-        Reviewing
-      </button>
-    </li>
-    <li className={styles.statusItem}>
-      <button
-        type="button"
-        className={styles.statusButton}
-        onClick={() => updateTaskStatus('COMPLETE')}>
-        Complete
-      </button>
-    </li>
+export const TaskStatusList: React.VFC = () => (
+  <ul className={styles.root}>
+    <StatusListButton label="New" status="NEW" />
+    <StatusListButton label="WIP" status="IN_PROGRESS" />
+    <StatusListButton label="Reviewing" status="REVIEWING" />
+    <StatusListButton label="Complete" status="COMPLETE" />
   </ul>
 );
--- client/src/components/TaskStatusList/index.tsx
+++ client/src/components/TaskStatusList/index.tsx
@@ -1,29 +1,4 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectTask, TaskStatus } from '../../redux/modules/task';
-import { update } from '../../redux/modules/tasks';
 import { TaskStatusList as Presentational } from './TaskStatusList';
 
-export const TaskStatusList: React.VFC = () => {
-  const dispatch = useDispatch();
-  const task = useSelector(selectTask);
-
-  const updateTaskStatus = (status: TaskStatus) => {
-    dispatch(
-      update({
-        id: task.id!,
-        projectId: task.projectId,
-        title: task.title,
-        startDate: task.startDate,
-        dueDate: task.dueDate,
-        description: task.description,
-        status,
-        userId: task.userId!,
-        assignTo: task.assignTo,
-        createdAt: task.createdAt!,
-        updatedAt: task.updatedAt!,
-      }),
-    );
-  };
-  return <Presentational updateTaskStatus={updateTaskStatus} />;
-};
+export const TaskStatusList: React.VFC = () => <Presentational />;
