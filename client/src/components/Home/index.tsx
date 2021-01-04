ff9c45f840ad7884bea3b056289beee0bde93311
Shun Nakano <nakano_syun@waku-2.com>
2021-01-04 16:30:02 +0900

    refactor: GetStartedLink / LogoTopの適用

--- client/src/components/Home/index.tsx
+++ client/src/components/Home/index.tsx
@@ -1,24 +1,15 @@
-import Link from 'next/link';
 import React from 'react';
+import { GetStartedLink } from '../_atoms/GetStartedLink';
+import { LogoTop } from '../_atoms/LogoTop';
 import styles from './index.module.scss';
 
 export const Home: React.VFC = () => (
   <section className={styles.home}>
     <div className={styles.inner}>
-      <img src="/images/logo_l.png" alt="TASKSHAREのlogo" />
+      <LogoTop />
     </div>
     <div className={styles.linkWrapper}>
-      <Link href="/signup">
-        <a className={styles.link}>
-          <img
-            src="/images/icon-login.svg"
-            alt="Get started"
-            width="40"
-            height="40"
-          />
-          GET STARTED
-        </a>
-      </Link>
+      <GetStartedLink />
     </div>
   </section>
 );
