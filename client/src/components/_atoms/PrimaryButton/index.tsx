40e6ec91fd64f6d228adc386009c501046f2d4a3
Shun Nakano <nakano_syun@waku-2.com>
2021-01-04 14:27:03 +0900

    refactor: PrimaryButtonコンポーネントの作成

---
+++ client/src/components/_atoms/PrimaryButton/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+type Props = {
+  label: string;
+  type: 'button' | 'submit';
+  disabled: boolean;
+  arg?: string;
+  handleClick?: (() => void) | ((_arg: string) => void);
+};
+export const PrimaryButton: React.VFC<Props> = ({
+  label,
+  type,
+  disabled,
+  arg,
+  handleClick,
+}) => (
+  <>
+    {arg && handleClick ? (
+      <button
+        className={styles.root}
+        type={type}
+        disabled={disabled}
+        onClick={() => handleClick(arg)}>
+        {label}
+      </button>
+    ) : (
+      <button className={styles.root} type={type} disabled={disabled}>
+        {label}
+      </button>
+    )}
+  </>
+);
