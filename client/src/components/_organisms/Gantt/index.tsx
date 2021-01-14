7e76a4c876c44240bf2f97ec67680ea0ad52dfc6
Shun Nakano <nakano_syun@waku-2.com>
2021-01-14 16:23:58 +0900

    refactor: コンポーネントの適用

--- client/src/components/_organisms/Gantt/index.tsx
+++ client/src/components/_organisms/Gantt/index.tsx
@@ -1,59 +1,45 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { withAuth } from '../../../helpers/withAuth';
-import { selectOpenProjects } from '../../../redux/modules/projects';
-import { selectTasks, getRelatedTasks } from '../../../redux/modules/tasks';
-import { getUsername, selectUsers } from '../../../redux/modules/users';
+import {
+  getProjects,
+  selectOpenProjects,
+} from '../../../redux/modules/projects';
+import { selectOpenTasks, getTasks } from '../../../redux/modules/tasks';
 import styles from './index.module.scss';
+import { getInstance } from '../../../libs/db/getInstance';
+import { Project } from '../../../redux/modules/project';
+import { Task } from '../../../redux/modules/task';
+import { GanttChart } from '../../_molecules/GanttChart';
+import { GanttTaskList } from '../../_molecules/GanttTaskList';
+import { Heading } from '../../_molecules/Heading';
 
 const Component: React.VFC = () => {
+  const dispatch = useDispatch();
+  const db = getInstance();
   const projects = useSelector(selectOpenProjects);
-  const tasks = useSelector(selectTasks);
-  const users = useSelector(selectUsers);
+  const tasks = useSelector(selectOpenTasks);
+
+  useEffect(() => {
+    db.collection('projects').onSnapshot((snapshot) => {
+      const items: Project[] = [];
+      snapshot.forEach((doc) => items.push(doc.data() as Project));
+      dispatch(getProjects(items));
+    });
+    db.collection('tasks').onSnapshot((snapshot) => {
+      const items: Task[] = [];
+      snapshot.forEach((doc) => items.push(doc.data() as Task));
+      dispatch(getTasks(items));
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <section className={styles.root}>
+      <Heading title="Gantt Chart" />
       <div className={styles.wrapper}>
-        <div className={styles.content}>
-          <div className={styles.contentLeft}>
-            <div className={styles.heading}>
-              <div className={styles.headingTitle}>Title</div>
-              <div className={styles.headingUsername}>Username</div>
-            </div>
-            <ul>
-              {projects.map((project) => (
-                <>
-                  <li className={styles.row} key={project.id}>
-                    <div className={styles.rowTitle}>{project.title}</div>
-                    <div className={styles.rowUsername}>
-                      {getUsername(users, project.ownerId)}
-                    </div>
-                  </li>
-                  {getRelatedTasks(tasks, project.id!).map((task) => (
-                    <li className={styles.row} key={task.id}>
-                      <div className={styles.rowTitle}>
-                        <img
-                          src="/images/icon-corner-down-right.svg"
-                          alt=""
-                          width="24"
-                          height="24"
-                          className={styles.rowImg}
-                        />
-                        {task.title}
-                      </div>
-                      <div className={styles.rowUsername}>
-                        {getUsername(users, task.assignTo)}
-                      </div>
-                    </li>
-                  ))}
-                </>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.contentRight}>
-            <div className={styles.heading}></div>
-          </div>
-        </div>
+        <GanttTaskList projects={projects} tasks={tasks} />
+        <GanttChart projects={projects} tasks={tasks} />
       </div>
     </section>
   );
