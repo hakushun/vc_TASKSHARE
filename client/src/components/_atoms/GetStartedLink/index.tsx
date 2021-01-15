38906ef0c87232959cc84fe41a031ab893a5dcd3
Shun Nakano <nakano_syun@waku-2.com>
2021-01-15 16:20:32 +0900

    refactor: 不要なalt属性削除

--- client/src/components/_atoms/GetStartedLink/index.tsx
+++ client/src/components/_atoms/GetStartedLink/index.tsx
@@ -5,12 +5,7 @@ import styles from './index.module.scss';
 export const GetStartedLink: React.VFC = () => (
   <Link href="/signup">
     <a className={styles.root}>
-      <img
-        src="/images/icon-login.svg"
-        alt="Get started"
-        width="40"
-        height="40"
-      />
+      <img src="/images/icon-login.svg" alt="" width="40" height="40" />
       GET STARTED
     </a>
   </Link>
