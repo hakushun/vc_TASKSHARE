2fbaf71e3619854d75ae0541969d15bc2c31e7f5
Shun Nakano <nakano_syun@waku-2.com>
2021-01-15 16:19:20 +0900

    refactor: Formのアクセシビリティ改善

--- client/src/components/_molecules/FormWrapper/index.tsx
+++ client/src/components/_molecules/FormWrapper/index.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 import styles from './index.module.scss';
 
 type Props = {
+  id: string;
   title: string;
+  titleRef: React.MutableRefObject<HTMLHeadingElement | null>;
   onSubmit: (_event: React.FormEvent<HTMLFormElement>) => void;
 };
-export const FormWrapper: React.FC<Props> = ({ title, onSubmit, children }) => (
-  <form onSubmit={onSubmit} className={styles.root}>
+export const FormWrapper: React.FC<Props> = ({
+  id,
+  title,
+  titleRef,
+  onSubmit,
+  children,
+}) => (
+  <form onSubmit={onSubmit} id={id} className={styles.root}>
     <fieldset>
       <legend>
-        <h2 className={styles.title}>{title}</h2>
+        <h2 className={styles.title} ref={titleRef} tabIndex={-1}>
+          {title}
+        </h2>
       </legend>
     </fieldset>
     {children}
