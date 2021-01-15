73d9bbce05b717fcc1a13ef3cb3f0de9f74c7b49
Shun Nakano <nakano_syun@waku-2.com>
2021-01-15 16:18:31 +0900

    refactor: Modalのアクセシビリティ改善

--- client/src/components/_molecules/ModalWrapper/index.tsx
+++ client/src/components/_molecules/ModalWrapper/index.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Overlay } from '../../_atoms/Overlay';
 import styles from './index.module.scss';
 
-export const ModalWrapper: React.FC = ({ children }) => (
-  <Overlay>
-    <section className={styles.root}>{children}</section>
-  </Overlay>
-);
+type Props = {
+  id: string;
+  handleClose: () => void;
+};
+export const ModalWrapper: React.FC<Props> = ({
+  id,
+  handleClose,
+  children,
+}) => {
+  const handleKeydown = (e: any) => {
+    if (e.key === 'Escape') handleClose();
+  };
+
+  useEffect(() => {
+    document.getElementById('app')?.setAttribute('aria-hidden', 'true');
+    document.body.addEventListener('keydown', handleKeydown);
+
+    return () => {
+      document.getElementById('app')?.removeAttribute('aria-hidden');
+      document.body.removeEventListener('keydown', handleKeydown);
+      document.getElementById('main')?.focus();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Overlay>
+      <section
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby={id}
+        className={styles.root}>
+        {children}
+      </section>
+    </Overlay>
+  );
+};
