50d718780af5e4abd106f96e452dd107efce5c34
Shun Nakano <nakano_syun@waku-2.com>
2021-01-15 16:16:05 +0900

    refactor: routing後にmainタグにfocusを当てるよう修正

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { useStore } from '../redux/configureStore';
 
 import '../components/stylesheets/reset.scss';
@@ -11,6 +12,18 @@ export default function App({
   pageProps,
 }: AppProps): React.ReactElement {
   const store = useStore(pageProps.initialReduxState);
+  const router = useRouter();
+
+  const handleRouteChange = () => {
+    const main = document.getElementById('main');
+    main?.focus();
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Provider store={store}>
