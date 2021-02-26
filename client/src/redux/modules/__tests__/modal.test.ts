438ced1b4f262f62c1d4e6de28767090e2ebc444
Shun Nakano <nakano_syun@waku-2.com>
2021-02-26 13:55:07 +0900

    test: modules/modalのテストケース追加

--- client/src/redux/modules/__tests__/modal.test.ts
+++ client/src/redux/modules/__tests__/modal.test.ts
@@ -13,6 +13,27 @@ import reducer, {
   toggleResetPasswordForm,
   toggleTaskForm,
 } from '../modal';
+import { add as addProject, edit as editProject } from '../project';
+import { add as addTask, edit as editTask } from '../task';
+import { add as addActivity, edit as editActivity } from '../activity';
+import {
+  createActions as createProjectActions,
+  updateActions as updateProjectActions,
+  removeActions as removeProjectActions,
+} from '../projects';
+import {
+  createActions as createTaskActions,
+  updateActions as updateTaskActions,
+  removeActions as removeTaskActions,
+} from '../tasks';
+import {
+  createActions as createActivityActions,
+  updateActions as updateActivityActions,
+  removeActions as removeActivityActions,
+} from '../activities';
+import {
+  removeActions as removeUserActions,
+} from '../users';
 
 describe('Reducer: modal', () => {
   it('Initial state', () => {
@@ -102,6 +123,315 @@ describe('Reducer: modal', () => {
       deleteForm: false,
       resetPasswordForm: false,
       confirmation: true,
+    });
+  });
+
+  it('Action: addProject', () => {
+    const action = addProject({ userId: '' });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: true,
+      taskForm: false,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: editProject', () => {
+    const action = editProject({ id: '' });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: true,
+      taskForm: false,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: addTask', () => {
+    const action = addTask({ userId: '' });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: true,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: editTask', () => {
+    const action = editTask({ id: '' });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: true,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: addActivity', () => {
+    const action = addActivity({ taskId: '', userId: '' });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: false,
+      activityForm: true,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: editActivity', () => {
+    const action = editActivity({ id: '' });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: false,
+      activityForm: true,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: createProjectActions.done', () => {
+    const action = createProjectActions.done({
+      params: {
+        title: '',
+        startDate: '',
+        dueDate: '',
+        ownerId: '',
+        detail: '',
+        userId: '',
+      },
+      result: {
+        title: '',
+        ownerId: '',
+        startDate: '',
+        dueDate: '',
+        detail: '',
+      },
+    });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: false,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: updateProjectActions.done', () => {
+    const action = updateProjectActions.done({
+      params: {
+        id: '',
+        title: '',
+        startDate: '',
+        dueDate: '',
+        ownerId: '',
+        detail: '',
+        userId: '',
+        createdAt: 0,
+        updatedAt: 0,
+      },
+      result: {
+        title: '',
+        ownerId: '',
+        startDate: '',
+        dueDate: '',
+        detail: '',
+      },
+    });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: false,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: removeProjectActions.done', () => {
+    const action = removeProjectActions.done({
+      params: {
+        id: '',
+      },
+      result: {
+        id: '',
+      },
+    });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: false,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: createTaskActions.done', () => {
+    const action = createTaskActions.done({
+      params: {
+        projectId: '',
+        title: '',
+        assignTo: '',
+        startDate: '',
+        dueDate: '',
+        description: '',
+        status: 'NEW',
+        userId: '',
+      },
+      result: undefined,
+    });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: false,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: updateTaskActions.done', () => {
+    const action = updateTaskActions.done({
+      params: {
+        id: '',
+        projectId: '',
+        title: '',
+        assignTo: '',
+        startDate: '',
+        dueDate: '',
+        description: '',
+        status: 'NEW',
+        userId: '',
+        createdAt: 0,
+        updatedAt: 0,
+      },
+      result: undefined,
+    });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: false,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: removeTaskActions.done', () => {
+    const action = removeTaskActions.done({
+      params: {
+        id: '',
+      },
+      result: undefined,
+    });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: false,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: createActivityActions.done', () => {
+    const action = createActivityActions.done({
+      params: {
+        taskId: '',
+        comment: '',
+        userId: '',
+      },
+      result: undefined,
+    });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: false,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: updateActivityActions.done', () => {
+    const action = updateActivityActions.done({
+      params: {
+        id: '',
+        taskId: '',
+        comment: '',
+        userId: '',
+        createdAt: 0,
+        updatedAt: 0,
+      },
+      result: undefined,
+    });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: false,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: removeActivityActions.done', () => {
+    const action = removeActivityActions.done({
+      params: {
+        id: '',
+      },
+      result: undefined,
+    });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: false,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
+    });
+  });
+
+  it('Action: removeUserActions.done', () => {
+    const action = removeUserActions.done({
+      params: {
+        id: '',
+      },
+      result: undefined,
+    });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      projectForm: false,
+      taskForm: false,
+      activityForm: false,
+      deleteForm: false,
+      resetPasswordForm: false,
+      confirmation: false,
     });
   });
 });
