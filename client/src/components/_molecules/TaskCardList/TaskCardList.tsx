16d0ac6f4118da42a4483df9d488620e70f3ee0e
Shun Nakano <hakushun.pianist@gmail.com>
2021-03-24 21:05:59 +0900

    feat: TaskCardListにhandleFocus実装＋スタイル実装

--- client/src/components/_molecules/TaskCardList/TaskCardList.tsx
+++ client/src/components/_molecules/TaskCardList/TaskCardList.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import React from 'react';
+import { getStringDate } from '../../../libs/date';
 import { toStringStatus } from '../../../libs/utils';
 import { Task, TaskStatus } from '../../../redux/modules/task';
 import styles from './index.module.scss';
@@ -6,15 +8,33 @@ import styles from './index.module.scss';
 type Props = {
   status: TaskStatus;
   assignedTasks: Task[];
+  handleFocus: (_taskId: string, _projectId: string) => void;
 };
-export const TaskCardList: React.VFC<Props> = ({ status, assignedTasks }) => (
+export const TaskCardList: React.VFC<Props> = ({
+  status,
+  assignedTasks,
+  handleFocus,
+}) => (
   <div className={styles.root}>
-    <h3>{toStringStatus(status)}</h3>
-    <ul>
+    <h3 className={styles.status}>{toStringStatus(status)}</h3>
+    <ul className={styles.list}>
       {assignedTasks
         .filter((task) => task.status === status)
         .map((task) => (
-          <li key={task.id}>{task.title}</li>
+          <li key={task.id} className={styles.card}>
+            <Link href={`/tasks/${task.id}`}>
+              <a
+                id={`tasks_${task.id}`}
+                aria-label={`タスク名：${task.title}、ステータス：${
+                  task.status
+                }、期限${getStringDate(task.dueDate)}`}
+                className={styles.link}
+                onClick={() => handleFocus(task.id!, task.projectId)}
+                onKeyPress={() => handleFocus(task.id!, task.projectId)}>
+                {task.title}
+              </a>
+            </Link>
+          </li>
         ))}
     </ul>
   </div>
--- client/src/components/_molecules/TaskCardList/index.tsx
+++ client/src/components/_molecules/TaskCardList/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Task, TaskStatus } from '../../../redux/modules/task';
+import { useDispatch } from 'react-redux';
+import { focus, Task, TaskStatus } from '../../../redux/modules/task';
 import { TaskCardList as Presentational } from './TaskCardList';
 
 type Props = {
   status: TaskStatus;
   assignedTasks: Task[];
 };
-export const TaskCardList: React.VFC<Props> = ({ status, assignedTasks }) => (
-  <Presentational status={status} assignedTasks={assignedTasks} />
-);
+export const TaskCardList: React.VFC<Props> = ({ status, assignedTasks }) => {
+  const dispatch = useDispatch();
+
+  const handleFocus = (taskId: string, projectId: string) => {
+    dispatch(focus({ id: taskId, projectId }));
+  };
+
+  return (
+    <Presentational
+      status={status}
+      assignedTasks={assignedTasks}
+      handleFocus={handleFocus}
+    />
+  );
+};
