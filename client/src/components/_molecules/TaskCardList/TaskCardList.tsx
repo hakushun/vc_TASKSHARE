3bcf455e1411511be93733e415f4cccfe944fc60
Shun Nakano <nakano_syun@waku-2.com>
2021-03-25 19:39:26 +0900

    feat: TaskCardListにuseDrop実装

--- client/src/components/_molecules/TaskCardList/TaskCardList.tsx
+++ client/src/components/_molecules/TaskCardList/TaskCardList.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import clsx from 'clsx';
+import { ConnectDropTarget } from 'react-dnd';
 import { toStringStatus } from '../../../libs/utils';
 import { Task, TaskStatus } from '../../../redux/modules/task';
 import { TaskCard } from '../TaskCard';
@@ -7,9 +9,16 @@ import styles from './index.module.scss';
 type Props = {
   status: TaskStatus;
   assignedTasks: Task[];
+  isOver: boolean;
+  drop: ConnectDropTarget;
 };
-export const TaskCardList: React.VFC<Props> = ({ status, assignedTasks }) => (
-  <div className={styles.root}>
+export const TaskCardList: React.VFC<Props> = ({
+  status,
+  assignedTasks,
+  isOver,
+  drop,
+}) => (
+  <div className={clsx(styles.root, isOver && styles.isOver)} ref={drop}>
     <h3 className={styles.status}>{toStringStatus(status)}</h3>
     <ul className={styles.list}>
       {assignedTasks
--- client/src/components/_molecules/TaskCardList/index.tsx
+++ client/src/components/_molecules/TaskCardList/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { useDrop } from 'react-dnd';
+import { useDispatch } from 'react-redux';
+import { AnyAction } from 'redux';
+import { StepAction } from 'redux-effects-steps';
 import { Task, TaskStatus } from '../../../redux/modules/task';
+import { update, UpdatePayload } from '../../../redux/modules/tasks';
 import { TaskCardList as Presentational } from './TaskCardList';
 
 type Props = {
   status: TaskStatus;
   assignedTasks: Task[];
 };
-export const TaskCardList: React.VFC<Props> = ({ status, assignedTasks }) => (
-  <Presentational status={status} assignedTasks={assignedTasks} />
-);
+export const TaskCardList: React.VFC<Props> = ({ status, assignedTasks }) => {
+  const dispatch = useDispatch();
+
+  const [{ isOver }, drop] = useDrop<UpdatePayload, StepAction<AnyAction>, {
+    isOver: boolean;
+}>(() => ({
+    accept: 'TASKCARD',
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+    }),
+    drop: (item) => dispatch(update({...item, status})),
+  }));
+
+  return (
+    <Presentational
+      status={status}
+      assignedTasks={assignedTasks}
+      isOver={isOver}
+      drop={drop}
+    />
+  );
+};
