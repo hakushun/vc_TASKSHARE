5cfb05257661154e8e2588bb684fb99df6dada66
Shun Nakano <nakano_syun@waku-2.com>
2021-03-25 19:38:23 +0900

    feat: DndProvider設置

--- client/src/components/_organisms/TaskBoard/index.tsx
+++ client/src/components/_organisms/TaskBoard/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
 import { Task } from '../../../redux/modules/task';
 import { TaskBoard as Presentational } from './TaskBoard';
 
@@ -6,5 +8,7 @@ type Props = {
   assignedTasks: Task[];
 };
 export const TaskBoard: React.VFC<Props> = ({ assignedTasks }) => (
-  <Presentational assignedTasks={assignedTasks} />
+  <DndProvider backend={HTML5Backend}>
+    <Presentational assignedTasks={assignedTasks} />
+  </DndProvider>
 );
